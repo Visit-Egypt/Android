7f503b82bd73d9dc3c5bf0e9880e9397fdf1e0a0
alaagawish <53160167+alaagawish@users.noreply.github.com>
2022-01-06 23:21:12 +0200

    chatbot button

--- app/src/main/java/com/visitegypt/ui/setting/SettingFragment.java
+++ app/src/main/java/com/visitegypt/ui/setting/SettingFragment.java
@@ -17,4 +17,12 @@ public class SettingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_setting, container, false);
     }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+
+    }
 }
